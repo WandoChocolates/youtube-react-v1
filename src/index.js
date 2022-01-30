83634796c739205cd7c0f21f97e0f5d8938aa559
WandoChocolates <ellytitus50@gmail.com>
2022-01-30 22:17:53 +0800

    05 semantic ui icons work update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import 'semantic-ui-css/semantic.min.css';
+import "semantic-ui-css/semantic.min.css";
 
 import App from "./App";
+
+/* Or you can use the cdn to use Semantic UI icons */
+const styleLink = document.createElement("link");
+styleLink.rel = "stylesheet";
+styleLink.href =
+  "https://cdn.jsdelivr.net/npm/semantic-ui/dist/semantic.min.css";
+document.head.appendChild(styleLink);
+/* end Semantic UI cdn include */
 
 ReactDOM.render(<App />, document.getElementById("root"));
