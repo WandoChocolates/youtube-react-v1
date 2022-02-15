6bfc5b5a7a68c6bde7aec8fa911971e78009e617
WandoChocolates <ellytitus50@gmail.com>
2022-02-15 21:50:43 +0800

    09

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
-import HeaderNav from "./containers/HeaderNav/HeaderNav";
+import { AppLayout } from "./components/AppLayout/AppLayout";
 import { Home } from "./containers/Home/Home";
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
-        <HeaderNav />
-        <Home />
+        <AppLayout>
+          <Home />
+        </AppLayout>
       </React.Fragment>
     );
   }
