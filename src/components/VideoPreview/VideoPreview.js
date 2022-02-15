d17ed29aeb55f7e6cec63395c41bcff2a8756b4a
WandoChocolates <ellytitus50@gmail.com>
2022-02-15 15:44:10 +0800

    07 and 08 VideoPreview bug fix

--- src/components/VideoPreview/VideoPreview.js
+++ src/components/VideoPreview/VideoPreview.js
@@ -11,13 +11,13 @@ export class VideoPreview extends React.Component {
           <div className="time-label">
             <span>05:22</span>
           </div>
-          <div className="video-info">
-            <div className="semi-bold show-max-two-lines">Video title</div>
-            <div className="video-preview-metadata-container">
-              <div className="channel-title">Channel title</div>
-              <div>
-                <span>2.1M views • 2 days ago</span>
-              </div>
+        </div>
+        <div className="video-info">
+          <div className="semi-bold show-max-two-lines">Video title</div>
+          <div className="video-preview-metadata-container">
+            <div className="channel-title">Channel title</div>
+            <div>
+              <span>2.1M views • 2 days ago</span>
             </div>
           </div>
         </div>
